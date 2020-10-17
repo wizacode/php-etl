590fc6c6c361bdefc17151cca828d084a76acb4f
Karl DeBisschop <karl.debisschop@bioraft.com>
2020-10-17 00:26:11 -0400

    Add ability to handle comparisons other than equality in Table extraction where operator.

--- tests/Extractors/TableTest.php
+++ tests/Extractors/TableTest.php
@@ -12,6 +12,9 @@ declare(strict_types=1);
 namespace Tests\Extractors;
 
 use Tests\TestCase;
+use Wizaplace\Etl;
+use Wizaplace\Etl\Database\ConnectionFactory;
+use Wizaplace\Etl\Database\Manager;
 use Wizaplace\Etl\Extractors\Table;
 use Wizaplace\Etl\Row;
 
@@ -64,5 +67,62 @@ class TableTest extends TestCase
         ]);
 
         static::assertEquals([new Row(['row1']), new Row(['row2'])], iterator_to_array($extractor->extract()));
+    }
+
+    /**
+     * Tests extended where-clause comparisons (e.g., <>, <, >, <=, >=).
+     *
+     * @param array $expected The expected result of table extraction.
+     * @param string|string[] $where The where clause used in filtering.
+
+     * @test
+     * @dataProvider whereClauseDataProvider
+     */
+    public function whereClauseOperators(array $expected, $where)
+    {
+        $name = tempnam(sys_get_temp_dir(), 'etl');
+        $config = [
+            'driver' => 'sqlite',
+            'database' => $name,
+        ];
+        $factory = new ConnectionFactory();
+        $manager = new Manager($factory);
+        $manager->addConnection($config);
+        $database = $manager->pdo('default');
+        $database->exec('CREATE TABLE unit (column VARCHAR(20))');
+        $database->exec('INSERT INTO unit VALUES ("row1")');
+        $database->exec('INSERT INTO unit VALUES ("row2")');
+
+        $extractor = new Table($manager);
+        $etl = new Etl\Etl();
+        $options = [
+            'connection' => 'default',
+            'columns' => ['column'],
+            'where' => ['column' => $where],
+        ];
+        $array = $etl->extract($extractor, 'unit', $options);
+        self::assertEquals($expected, $array->toArray());
+
+        // Clean up our temporary database.
+        unlink($name);
+    }
+
+    /**
+     * Provides test case scenarios for {@see whereClauseOperators()}.
+     *
+     * @return array A list of test case scenarios.
+     */
+    public function whereClauseDataProvider(): array
+    {
+        return [
+            [[], ['<', 'row1']],
+            [[['column' => 'row1']], 'row1'],
+            [[['column' => 'row1']], ['=', 'row1']],
+            [[['column' => 'row2']], ['>', 'row1']],
+            [[['column' => 'row2']], ['>=', 'row2']],
+            [[['column' => 'row1']], ['<>', 'row2']],
+            [[['column' => 'row1'], ['column' => 'row2']], ['<=', 'row2']],
+            [[['column' => 'row1'], ['column' => 'row2']], ['<>', 'row3']],
+        ];
     }
 }
