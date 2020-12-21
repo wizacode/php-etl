6318892da2c140aea92d56f4ce262404afe8100d
Karl DeBisschop <kdebisschop@gmail.com>
2020-12-21 17:42:45 -0500

    update pre-commit hook, fix invalid test method name missed in rebase

--- tests/Loaders/InsertUpdateTest.php
+++ tests/Loaders/InsertUpdateTest.php
@@ -244,13 +244,13 @@ class InsertUpdateTest extends TestCase
     }
 
     /** @test */
-    public function empty_filter_on_columns_to_insert()
+    public function emptyFilterOnColumnsToInsert(): void
     {
-        $this->select->expects($this->once())->method('fetch')->willReturn(false);
+        $this->select->expects(static::once())->method('fetch')->willReturn(false);
 
-        $this->statement->expects($this->once())->method('insert')
+        $this->statement->expects(static::once())->method('insert')
             ->with('table', ['id', 'name', 'email']);
-        $this->insert->expects($this->once())->method('execute')
+        $this->insert->expects(static::once())->method('execute')
             ->with(['id' => '1', 'name' => 'Jane Doe', 'email' => 'janedoe@example.com']);
 
         $this->loader->options(['columns' => []]);
